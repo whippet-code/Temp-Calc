ed9babaa2006f7e951cb1d6328e380f8b342bce6
Mark Ivkovic <markivkovicphotography@gmail.com>
2020-06-26 19:21:47 +0100

    Input / Output functionality

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Want all INPUTS and OUTPUTS rounded to nearest degree.
+var tempC = document.getElementById('tempC').value;
+function cToK() {
+    var tempC = document.getElementById('tempC').value;
+    let temp = Math.round(tempC);
+    temp = Math.round(temp + 273);
+    let out = document.getElementById('output')
+    out.innerHTML = temp
+};
